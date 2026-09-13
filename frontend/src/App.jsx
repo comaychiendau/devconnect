8e6bcd2a5dfb536e79df09c63d80ddfdcd6da906
comaychiendau <a1902421@adelaide.edu.au>
2026-09-13 22:14:15 +0930

    logic update: allow non-user to visit Communities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,25 +4,23 @@ import CommunitiesPage from './pages/CommunitiesPage.jsx'
 import GuestHomePage from './pages/GuestHomePage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import SignUpPage from './pages/SignUpPage.jsx'
-import ProtectedRoute from './components/ProtectedRoute.jsx'
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<GuestHomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route
-        path="/communities"
-        element={
-          <ProtectedRoute>
-            <CommunitiesPage />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<GuestHomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route
+                path="/communities"
+                element={<CommunitiesPage />}
+            />
+            <Route
+                path="*"
+                element={<Navigate to="/" replace />}
+            />
+        </Routes>
+    )
 }
 
 export default App
